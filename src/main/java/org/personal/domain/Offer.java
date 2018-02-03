7d201ecf99ed7d9ce25704e5a8352e8cdfe8e8d9
Gabriel Padurean <gabriel.padurean@paddypowerbetfair.com>
2018-02-03 18:54:02 +0200

    Add domain classes and testing dependencies

---
+++ src/main/java/org/personal/domain/Offer.java
@@ -0,0 +1,86 @@
+package org.personal.domain;
+
+import java.util.Currency;
+import java.util.Date;
+
+/**
+ * @author gabrielpadurean
+ */
+public class Offer {
+    private long id;
+
+    private String name;
+    private String description;
+    private boolean enabled;
+
+    private double price;
+    private Currency currency;
+
+    private Date startDate;
+    private Date endDate;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setActive(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
