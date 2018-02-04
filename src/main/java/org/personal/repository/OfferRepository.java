c429d2ed7fb6f84bdad76e3e2f385be494f67526
Gabriel Padurean <gabriel.padurean@paddypowerbetfair.com>
2018-02-05 00:47:52 +0200

    Fix selection of valid offers

--- src/main/java/org/personal/repository/OfferRepository.java
+++ src/main/java/org/personal/repository/OfferRepository.java
@@ -6,17 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.List;
-
 /**
  * @author gabrielpadurean
  */
 public interface OfferRepository extends PagingAndSortingRepository<Offer, Long> {
 
-    /**
-     * TODO: implement also selection based on date
-     */
-    @Query(value = "select offer from Offer offer where offer.enabled = true",
-            countQuery = "select count(offer) from Offer offer where offer.enabled = true")
+    @Query(value = "select offer from Offer offer where offer.enabled = true and offer.startDate <= current_date and offer.endDate >= current_date",
+            countQuery = "select count(offer) from Offer offer where offer.enabled = true and offer.startDate <= current_date and offer.endDate >= current_date")
     Page<Offer> findAllActive(Pageable pageable);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,15 @@
-INSERT INTO offer (id, product_id, name, description, enabled, price, currency, start_date, end_date) VALUES (1, 1009, 'First offer', 'Description for the first offer', true, 20, 'RON', '2018-01-01 14:00:00', '2019-01-01 14:00:00');
-INSERT INTO offer (id, product_id, name, description, enabled, price, currency, start_date, end_date) VALUES (2, 1010, 'Second offer', 'Description for the second offer', true, 15.20, 'EUR', '2017-06-25 18:30:00', '2018-07-15 15:00:00');
-INSERT INTO offer (id, product_id, name, description, enabled, price, currency, start_date, end_date) VALUES (3, 1011, 'Third offer', 'Description for the third offer', false, 0.50, 'EUR', '2018-01-01 14:00:00', '2019-01-01 14:00:00');
-INSERT INTO offer (id, product_id, name, description, enabled, price, currency, start_date, end_date) VALUES (4, 1009, 'Forth offer', 'Description for the forth offer', true, 1.50, 'RON', '2016-01-01 14:00:00', '2017-01-01 14:00:00');
+INSERT INTO offer (id, product_id, name, description, enabled, price, currency, start_date, end_date) VALUES
+  (1, 1009, 'First offer', 'Description for the first offer', TRUE, 20, 'RON', '2018-01-01 14:00:00',
+   '2019-01-01 14:00:00');
+INSERT INTO offer (id, product_id, name, description, enabled, price, currency, start_date, end_date) VALUES
+  (2, 1010, 'Second offer', 'Description for the second offer', TRUE, 15.20, 'EUR', '2017-06-25 18:30:00',
+   '2018-07-15 15:00:00');
+INSERT INTO offer (id, product_id, name, description, enabled, price, currency, start_date, end_date) VALUES
+  (3, 1011, 'Third offer', 'Description for the third offer', FALSE, 0.50, 'EUR', '2018-01-01 14:00:00',
+   '2019-01-01 14:00:00');
+INSERT INTO offer (id, product_id, name, description, enabled, price, currency, start_date, end_date) VALUES
+  (4, 1009, 'Forth offer', 'Description for the forth offer', TRUE, 1.50, 'RON', '2016-01-01 14:00:00',
+   '2017-01-01 14:00:00');
+INSERT INTO offer (id, product_id, name, description, enabled, price, currency, start_date, end_date) VALUES
+  (5, 1009, 'Fifth offer', 'Description for the fifth offer', TRUE, 1500, 'RON', '2018-01-01 14:00:00',
+   '2019-01-01 14:00:00');
